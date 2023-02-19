3edef8e1ac3b97d4a6e06db6260249db111ef759
shiv <yadav.shivkumar5@gmail.com>
2023-02-19 21:34:35 +0530

    vector part 3

---
+++ vector/vector_part_3.cpp
@@ -0,0 +1,34 @@
+/*
+std::vector
+size()                    : returns the number of elements
+resize(size_type count)   : changes the number of elements stored
+*/
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> mVect;
+
+    for (int i = 0; i <= 5; i++)
+        mVect.push_back(i);
+
+    std::cout << "vector size(): " << mVect.size() << std::endl; // 6
+
+    for (auto i = mVect.begin(); i != mVect.end(); ++i)
+        std::cout << *i << " ";
+
+    std::cout << std::endl;
+
+    mVect.resize(4);
+
+    std::cout << "vector size(): " << mVect.size() << std::endl; // 4
+
+    for (auto i = mVect.begin(); i != mVect.end(); ++i)
+        std::cout << *i << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
