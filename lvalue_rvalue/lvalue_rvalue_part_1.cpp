a8348a54d664ff78b822862ce442a91b61b10980
shiv <yadav.shivkumar5@gmail.com>
2023-05-13 21:11:33 +0530

    lvalue rvalue part 1

---
+++ lvalue_rvalue/lvalue_rvalue_part_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/*
+
+lvalue : Named variables, which has address, it appears at left hand side of assignment operator
+rvalue : Which is not lvalue, it appears at right hand side of assignment operator
+
+*/
+
+int main()
+{
+
+int lvalue = (10 + 20); // (10+20) is rvalue, temporary value
+
+std::cout<<"address lvalue : "<<&lvalue<<std::endl;
+
+//error
+//std::cout<<"address rvalue :"<<&(10 + 20)<<std::endl;
+
+ return 0;   
+}
---
+++ pair/build/CMakeFiles/pair4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pair4.dir/pair_part_4.cpp.o"
+  "pair4.pdb"
+  "pair4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pair4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
