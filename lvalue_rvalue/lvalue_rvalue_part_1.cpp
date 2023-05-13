845f3ed5c97aa75d1037d89cc6a7fa33b004fd43
shiv <yadav.shivkumar5@gmail.com>
2023-05-13 21:32:07 +0530

    lvalue rvalue part 1

--- lvalue_rvalue/lvalue_rvalue_part_1.cpp
+++ lvalue_rvalue/lvalue_rvalue_part_1.cpp
@@ -14,7 +14,7 @@ int lvalue = (10 + 20); // (10+20) is rvalue, temporary value
 std::cout<<"address lvalue : "<<&lvalue<<std::endl;
 
 //compiler error
-std::cout<<"address rvalue :"<<&(10 + 20)<<std::endl;
+//std::cout<<"address rvalue :"<<&(10 + 20)<<std::endl;
 
  return 0;   
 }
